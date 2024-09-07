dd194ea1acfd53f7b652d0061ae03059f68c1307
Noah7-cyber <109804164+Noah7-cyber@users.noreply.github.com>
2024-09-07 12:05:07 +0100

    delete function not perfect

--- technotes/controllers/userController.js
+++ technotes/controllers/userController.js
@@ -4,10 +4,11 @@ const asyncHandler = require('express-async-handler');
 const bcrypt =require('bcrypt');
 
 const getAllUsers =asyncHandler( async(req, res)=>{
-    const users = await User.find().select("password").lean();
+    const users = await User.find().select('-password').lean();
     if(!users?.length){
         return res.status(400).json({messsage:'No Users found'});
     }
+    
     res.json(users);
 })
 const createNewUser = asyncHandler(async (req, res) => {
@@ -38,18 +39,19 @@ const createNewUser = asyncHandler(async (req, res) => {
     }
 });
 const updateUser =asyncHandler( async(req, res)=>{
-    const {id, username, roles, active, password} = req.body;
+    const {id, username, roles, active, password } = req.body;
     //confirm data
     if(!id || !username ||!Array.isArray(roles) ||!roles.length || typeof active !=='boolean'){
         return res.status(400).json({message:'All feilds are required'});
     }
     const user = await User.findById(id).exec();
+    console.log(user)
     if(!user){
         return res.status(400).json({message: 'User not found'});
     }
     //check for duplicate
-    const duplicate = await User.findOne({usernae}).lean().exec();
-    if(duplicate && duplicate ?._id.toString() !==id){
+    const duplicate = await User.findOne({username}).lean().exec();
+    if(duplicate && duplicate?._id.toString() !==id){
         return res.status(409).json({message:'Duplicate username'});
     }
 
@@ -69,19 +71,19 @@ const deleteUser =asyncHandler( async(req, res)=>{
     if(!id){
         return res.status(400).json({message:'User ID Required'})
     }
-    const notes = await Note.findOne({user:id}).lean().exec();
-    if(notes?.length){
+    const note = await Note.findOne({user:id}).lean().exec();
+    if(note){
         return res.status(400).json({message:'User has assigned notes'});
-
     }
     const user = await User.findById(id).exec();
     if(!user){
         return res.status(400).json({message:'User not found'})
+    }
        const result = await user.deleteOne();
-
+       
        const reply =`Username ${result.username} with ID ${result._id} deleted`;
        res.json(reply)
-    }
+    
 })
 module.exports ={
     getAllUsers,
